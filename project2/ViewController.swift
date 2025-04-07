0fa2e24af63d70b52e722efbd41c0daab0af66dd
godgiftedboy <rajprajapati12321@gmail.com>
2025-04-07 10:46:38 +0545

    chore: added border to buttons

--- project2/ViewController.swift
+++ project2/ViewController.swift
@@ -22,6 +22,23 @@ class ViewController: UIViewController {
         
         
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        button1.layer.borderWidth = 1;
+        button2.layer.borderWidth = 1;
+        button3.layer.borderWidth = 1;
+        
+        //CA (Core Animation) layer - core component of iOS's rendering system.
+        //Every UIView in iOS has an underlying CALayer that is responsible for rendering the view on screen.
+        // CALayer is the drawing engine behind every UIView.
+        // It handles graphics, animations, and effects like shadows and borders.
+        // You rarely need to use CALayer directly unless you want advanced visuals or custom animations
+        
+        //borderColor belongs to CA layer so .cgColor converts the UI color to CG (Core Graphics) compatible.
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor;
+        button2.layer.borderColor = UIColor.lightGray.cgColor;
+        button3.layer.borderColor = UIColor.lightGray.cgColor;
+        
         askQuestion();
     }
     
